853d9a1c00f03b122e6f5fa79b8c0baa797b0708
faseeh41 <rajafaseeh41@gmail.com>
2024-08-27 12:52:41 +0500

    first commit

---
+++ src/components/Credit.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+      <p>
+        Coded by
+        <a href="https://github.com/faseeh41"> M.Faseeh Hassan</a>,{" "}
+        <a href="https://github.com/faseeh41/REACT-QUIZAPP">
+          Open sourced on Github.
+        </a>{" "}
+        Hosted on Vercel
+      </p>
+    </div>
+  );
+}
